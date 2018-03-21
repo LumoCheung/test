d24dafc70aa4d1825f055027b6b6eb36fb393fa0
lumocheung <lumochueng@163.com>
2018-03-21 14:55:26 +0800

    java8

---
+++ test1.8/src/main/java/com/zlc/java8/Test.java
@@ -0,0 +1,17 @@
+package com.zlc.java8;
+
+/**
+ * @author zhangzilu02
+ * @create 2018/3/21
+ **/
+public class Test {
+
+    public static void main(String[] args){
+        TestClass test=new TestClass();
+
+//        test -> test.getA();
+        //https://segmentfault.com/a/1190000006985405#articleHeader1
+
+    }
+
+}
---
+++ test1.8/src/main/java/com/zlc/java8/TestClass.java
@@ -0,0 +1,16 @@
+package com.zlc.java8;
+
+import lombok.Data;
+
+/**
+ * @author zhangzilu02
+ * @create 2018/3/21
+ **/
+@Data
+public class TestClass {
+
+    private String a;
+
+    private String b;
+
+}
